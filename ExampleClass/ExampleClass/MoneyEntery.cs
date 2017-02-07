f9bccc32d8ad9a77489cf7a27df951d979f9cc2e
Egor Mikheev <Egor Mikheev>
2017-02-07 16:14:26 +0300

    Example Class

---
+++ ExampleClass/ExampleClass/MoneyEntery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleClass
+{
+    class MoneyEntery
+    {
+        private double _amount;
+        /// <summary>
+        /// Стандартный конструктор
+        /// </summary>
+
+        public MoneyEntery()
+        {
+            _amount = 0;
+            EnteryDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="amount">Сумма записи</param>
+        /// <param name="data">Дата записи</param>
+        public MoneyEntery(double amount, DateTime data)
+        {
+            _amount = amount;
+            EnteryDate = data;
+        }
+
+        /// <summary>
+        /// Инициализация объекта строками
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        public void InitWithString(string amount, string date)
+        {
+            Double.TryParse(amount, out _amount);
+            DateTime dt;
+            DateTime.TryParse(date, out dt);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} от {1}", _amount, EnteryDate.Date);
+        }
+
+
+        /// <summary>
+        /// Определяет является ли запись доходом
+        /// </summary>
+        public bool IsDebit
+        {
+            get { return (_amount >= 0); }
+            set { if(value && _amount < 0) _amount = -_amount;}
+        }
+
+        /// <summary>
+        /// Сумма записи
+        /// </summary>
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        /// <summary>
+        /// Дата записи
+        /// </summary>
+        public DateTime EnteryDate { get; set; }
+    }
+}
---
+++ ExampleClass/ExampleClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyEntery me = new MoneyEntery();
+
+            Console.Write("Hello");
+            Console.Write("ВВедите сумму: ");
+            string amount = Console.ReadLine();
+            Console.Write("Введите дату");
+            string date = Console.ReadLine();
+
+            me.InitWithString(amount, date);
+
+            Console.WriteLine("Ваша запись:");
+            Console.WriteLine(me.ToString());
+
+            if (me.IsDebit)
+                Console.WriteLine("У Вас новый доход");
+            else
+                Console.WriteLine("У Вас новый расход");
+
+            Console.ReadKey();
+        }
+    }
+}
