81f037682f3133af6bf0483b7cf8d46224d5e9b9
Egor Mikheev <Egor Mikheev>
2017-02-09 11:15:01 +0300

    Example OOP

---
+++ ExampleOOP/ExampleOOP/Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOOP
+{
+    public abstract class Car
+    {
+        protected int _speed;
+        protected string _type;
+
+        public Car()
+        {
+            _speed = 0;
+            _type = "noname";
+        }
+
+        public abstract void Drive();
+
+        public override string ToString()
+        {
+            return string.Format("Model: {0}; Max speed: {1}", _type, _speed);
+        }
+    }
+
+    public class Vaz:Car
+    {
+        public Vaz():base() 
+            {
+            _type = "VAZ 2105";
+            }
+        public override void Drive()
+        {
+            _speed = 60;
+        }
+    }
+
+    public class Lamborgini:Car,ITurbo
+    {
+
+        public Lamborgini():base() 
+            {
+            _type = "Lamborgini Galardo";
+        }
+        public override void Drive()
+        {
+            Turbo();
+            _speed = 260;
+        }
+
+        public void Turbo()
+        {
+
+        }
+    }
+
+    public class Ferrari:Car,IExtendedEngine
+    {
+        public Ferrari():base() 
+            {
+            _type = "Ferarri Enzo";
+        }
+        public override void Drive()
+        {
+            ExtendedEngine();
+            _speed = 240;
+        }
+
+        public void ExtendedEngine()
+        {
+
+        }
+
+        
+    }
+
+    interface ITurbo
+    {
+        void Turbo();
+    }
+    interface IExtendedEngine
+    {
+        void ExtendedEngine();
+    }
+}
---
+++ ExampleOOP/ExampleOOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите тип машины");
+            Console.WriteLine("VAZ - 2105");
+            Console.WriteLine("Lambo - Lamborgini");
+            Console.WriteLine("Ferarri - Ferarri");
+
+            string type = Console.ReadLine();
+
+            // полиморфизм
+            Car chosenCar = GetCarByType(type);
+
+            Console.WriteLine();
+            Console.WriteLine("Вы выбрали:");
+            Console.WriteLine(chosenCar.ToString());
+
+            Console.WriteLine("Поехали");
+            chosenCar.Drive();
+            Console.WriteLine(chosenCar.ToString());
+
+            if(chosenCar is ITurbo)
+            {
+                Console.WriteLine("Машина турбирована");
+                ((Lamborgini)chosenCar).Turbo();
+            }
+
+            Console.ReadKey();
+        }
+
+        static Car GetCarByType(string type)
+        {
+            switch(type)
+            {
+                case "VAZ":
+                    return new Vaz();
+                case "Lambo":
+                    return new Lamborgini();
+                case "Ferarri":
+                    return new Ferrari();
+                default:
+                    return null;
+            }
+        }
+    }
+}
