e06ec256014400fed69357e4b3afc10a8f42c075
Egor Mikheev <Egor Mikheev>
2017-02-13 20:12:39 +0300

    winForms

---
+++ ExampleWinForms/ExampleWinForms/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ExampleWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridViewEntries = new System.Windows.Forms.DataGridView();
+            this.ColumnType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEntries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAmount.Location = new System.Drawing.Point(96, 23);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(536, 22);
+            this.textBoxAmount.TabIndex = 0;
+            this.textBoxAmount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(522, 135);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(110, 30);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сумма";
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxBalance.Location = new System.Drawing.Point(95, 348);
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.ReadOnly = true;
+            this.textBoxBalance.Size = new System.Drawing.Size(537, 22);
+            this.textBoxBalance.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Balance";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpDate.Location = new System.Drawing.Point(96, 51);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(536, 22);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDescription.Location = new System.Drawing.Point(95, 79);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(537, 22);
+            this.textBoxDescription.TabIndex = 2;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCategory.Location = new System.Drawing.Point(95, 107);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(537, 22);
+            this.textBoxCategory.TabIndex = 3;
+            this.textBoxCategory.TextChanged += new System.EventHandler(this.textBoxCategory_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Дата";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Описание";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Категория";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // dataGridViewEntries
+            // 
+            this.dataGridViewEntries.AllowUserToAddRows = false;
+            this.dataGridViewEntries.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewEntries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEntries.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnType,
+            this.ColumnAmount,
+            this.ColumnDescription,
+            this.ColumnData,
+            this.ColumnCategory});
+            this.dataGridViewEntries.Location = new System.Drawing.Point(15, 171);
+            this.dataGridViewEntries.Name = "dataGridViewEntries";
+            this.dataGridViewEntries.RowTemplate.Height = 24;
+            this.dataGridViewEntries.Size = new System.Drawing.Size(617, 157);
+            this.dataGridViewEntries.TabIndex = 5;
+            this.dataGridViewEntries.TabStop = false;
+            this.dataGridViewEntries.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEntries_CellContentClick);
+            this.dataGridViewEntries.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEntries_CellValueChanged);
+            // 
+            // ColumnType
+            // 
+            this.ColumnType.HeaderText = "Тип записи";
+            this.ColumnType.Name = "ColumnType";
+            this.ColumnType.ReadOnly = true;
+            // 
+            // ColumnAmount
+            // 
+            this.ColumnAmount.HeaderText = "Сумма";
+            this.ColumnAmount.Name = "ColumnAmount";
+            // 
+            // ColumnDescription
+            // 
+            this.ColumnDescription.HeaderText = "Описание";
+            this.ColumnDescription.Name = "ColumnDescription";
+            // 
+            // ColumnData
+            // 
+            this.ColumnData.HeaderText = "Дата";
+            this.ColumnData.Name = "ColumnData";
+            // 
+            // ColumnCategory
+            // 
+            this.ColumnCategory.HeaderText = "Категория";
+            this.ColumnCategory.Name = "ColumnCategory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 386);
+            this.Controls.Add(this.dataGridViewEntries);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxAmount);
+            this.Name = "Form1";
+            this.Text = "Менеджер доходов";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEntries)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridViewEntries;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCategory;
+    }
+}
+
---
+++ ExampleWinForms/ExampleWinForms/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ExampleWinForms
+{
+    using MoneyEntry;
+    public partial class Form1 : Form
+    {
+
+        private double _balance;
+        public Form1()
+        {
+            InitializeComponent();
+            _balance = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void buttonAdd_Click(object sender, EventArgs e)
+        {
+            MoneyEntry me = new MoneyEntry();
+            double income;
+            double.TryParse(textBoxAmount.Text, out income);
+            me.Amount = income;
+            me.EntryDate = dtpDate.Value;
+            me.Description = textBoxDescription.Text;
+            me.Category = textBoxCategory.Text;
+
+            AddEntry(me);
+            UpdateBalance();
+            ClearFields();
+        }
+
+        private void UpdateBalance()
+        {
+            double balance = 0;
+
+            foreach(DataGridViewRow row in dataGridViewEntries.Rows)
+            {
+                double income;
+                double.TryParse((row.Cells[1].Value ?? "0").ToString(), out income);
+                balance += income;
+            }
+            textBoxBalance.Text = balance.ToString();
+        }
+
+        private void AddEntry(MoneyEntry me)
+        {
+            dataGridViewEntries.Rows.Add(me.IsDebit ? "Доход" : "Расход"
+                , me.Amount
+                , me.Description
+                , me.EntryDate.ToShortDateString()
+                , me.Category);
+        }
+
+        private void ClearFields()
+        {
+            textBoxAmount.Text = "";
+            textBoxDescription.Text = "";
+            textBoxCategory.Text = "";
+            dtpDate.Value = DateTime.Now;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewEntries_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewEntries_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1 &&  dataGridViewEntries.Rows.Count > 0)
+            {
+                double income;
+                double.TryParse((dataGridViewEntries[e.ColumnIndex, e.RowIndex].Value ?? "0").ToString(), out income);
+
+                dataGridViewEntries[0, e.RowIndex].Value = (income < 0) ? "Расход" : "Доход";
+                UpdateBalance();
+            }
+        }
+
+        private void textBoxCategory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
